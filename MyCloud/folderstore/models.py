ba54920e15bb01228fea254c413aed5e5ff7a25f
Agam Dua <agamdua@agamdua-HCL-Desktop.(none)>
2013-09-17 00:18:00 +0530

    Changed names in folderstore/models + added user
    1. Model name of files uploaded is now 'Resource'
    2. 'file' is keyword, so changed to 'file_upload'
    3. ManyToMany link between User and Resource (for file sharing later on)

--- MyCloud/folderstore/models.py
+++ MyCloud/folderstore/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 
-class FileStorage(models.Model):
+class Resource(models.Model):
     name=models.CharField(max_length=100)
-    file=models.FileField(upload_to='uploads/%Y/%m/%d')
+    file_upload=models.FileField(upload_to='uploads/%Y/%m/%d')
+    user = models.ManyToManyField(User)
+    # ManyToMany: file sharing between users will be a feature in the future
 
     def __unicode__(self):
-        return self.name
+        return self.name, self.username
